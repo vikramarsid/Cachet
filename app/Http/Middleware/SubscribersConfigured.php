e26fd9a0371df0a9099e9c1dbb84d3febce5859f
vikram-arsid <vikram.arsid@fireeye.com>
2020-06-07 23:01:10 -0400

    Debugging for invalid signature

--- app/Http/Middleware/SubscribersConfigured.php
+++ app/Http/Middleware/SubscribersConfigured.php
@@ -14,6 +14,9 @@ namespace CachetHQ\Cachet\Http\Middleware;
 use Closure;
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
 
 /**
  * This is the subscribers configured middleware class.
@@ -43,6 +46,40 @@ class SubscribersConfigured
     }
 
     /**
+     * Determine if the given request has a valid signature.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  bool  $absolute
+     * @return bool
+     */
+    public function hasValidSignature(Request $request, $absolute = true)
+    {
+        $url = $absolute ? $request->url() : '/'.$request->path();
+        Log::info("URL: ".$url);
+
+        $original = rtrim($url.'?'.Arr::query(
+                Arr::except($request->query(), 'signature')
+            ), '?');
+
+        Log::info("ORIGINAL: ".$original);
+
+        $expires = $request->query('expires');
+        Log::info("EXPIRES: ".$expires);
+
+        Log::info("APP_KEY: ".(string) $this->config->get("app")["key"]);
+
+        $signature = hash_hmac('sha256', $original, $this->config->get("app")["key"]);
+        Log::info("GENERATED_SIGNATURE: ".$signature);
+
+        Log::info("ORIGINAL_SIGNATURE: ".(string) $request->query('signature', ''));
+
+        Log::info("TIME_CHECK: ".(string) ! ($expires && Carbon::now()->getTimestamp() > $expires));
+
+        return  hash_equals($signature, (string) $request->query('signature', '')) &&
+            ! ($expires && Carbon::now()->getTimestamp() > $expires);
+    }
+
+    /**
      * Handle an incoming request.
      *
      * @param \Illuminate\Http\Request $request
@@ -52,6 +89,10 @@ class SubscribersConfigured
      */
     public function handle(Request $request, Closure $next)
     {
+        Log::info("============================== DEV LOGGING =====================================");
+        $status = $this->hasValidSignature($request);
+        Log::info("STATUS: ".$status);
+        Log::info("============================== DEV LOGGING =====================================");
         return $next($request);
     }
 }
